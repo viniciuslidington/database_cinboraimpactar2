14bdc9d17679569ec9fe02a86297c99eb985b104
Wesley Silveira <wslc@cin.ufpe.br>
2025-08-27 23:09:09 -0300

    feat: múltiplos databases c/ acesso de adm para o prisma efetuar migrates

---
+++ init-db.sql
@@ -0,0 +1,17 @@
+-- Criar database
+CREATE DATABASE connect_ong;
+
+-- Criar usuário
+CREATE USER connect_ong_user WITH PASSWORD 'connect_ong_pass';
+
+-- Dar permissões ao usuário para o database
+GRANT ALL PRIVILEGES ON DATABASE connect_ong TO connect_ong_user;
+
+-- Conceder permissões no schema public do connect_ong
+ALTER DATABASE connect_ong OWNER TO connect_ong_user;
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA public
+GRANT ALL PRIVILEGES ON TABLES TO connect_ong_user;
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA public
+GRANT ALL PRIVILEGES ON SEQUENCES TO connect_ong_user;
