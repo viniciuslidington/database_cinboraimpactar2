ff693dbe714b75564630a4169658a4fa76ac0dde
Wesley Silveira <wslc@cin.ufpe.br>
2025-08-27 23:12:36 -0300

    feat: permissão para criar a shadowdb do prisma

--- init-db.sql
+++ init-db.sql
@@ -4,6 +4,7 @@ CREATE DATABASE connect_ong;
 -- Criar usuário
 CREATE USER connect_ong_user WITH PASSWORD 'connect_ong_pass';
 
+ALTER USER connect_ong_user CREATEDB;
 -- Dar permissões ao usuário para o database
 GRANT ALL PRIVILEGES ON DATABASE connect_ong TO connect_ong_user;
 
@@ -15,3 +16,4 @@ GRANT ALL PRIVILEGES ON TABLES TO connect_ong_user;
 
 ALTER DEFAULT PRIVILEGES IN SCHEMA public
 GRANT ALL PRIVILEGES ON SEQUENCES TO connect_ong_user;
+
